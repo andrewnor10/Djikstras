83106060c39020e08768b0483c8810c526bd5c14
ANDREW NORTON <amnorton3@students.everettcc.edu>
2024-06-03 13:25:58 -0700

    commit

--- Graph.cpp
+++ Graph.cpp
@@ -7,4 +7,11 @@ Graph::Graph(int numVertices)
 	this->numVertices = numVertices;
 	
 }
+void Graph::addEdge(int SourceVertex, int DestinationVertex, int weight)
+{
+	
+}
+void Graph::dijkstra(int sourceVertex)
+{
 
+}
--- Graph.h
+++ Graph.h
@@ -3,11 +3,17 @@
 
 #include <vector>
 
-class Node {
+class Vertex {
 public:
     int value;
     int weight;
-    Node(int v, int w) : value(v), weight(w) {}
+    Vertex(int v, int w) : value(v), weight(w) {}
+};
+class Edge {
+    Vertex v1;
+    Vertex v2;
+    Edge(Vertex v1, Vertex v2) : v1(v1), v2(v2) {};
+
 };
 
 class Graph // Dijkstra's Algorithm
@@ -18,11 +24,12 @@ public:
     void addEdge(int sourceVertex, int destinationVertex, int weight);
     void dijkstra(int sourceVertex);
     void printSolution(std::vector<int>& distances, int numVertices);
+    bool adjacent(sourceVertex, destinationVertex);
     int minDistance(const std::vector<int>& distances, const std::vector<bool>& shortestPathTreeSet, int numVertices);
 
 private:
     int numVertices;
-    std::vector<std::vector<Node>> graph;
+    std::vector<std::vector<Vertex>> graph;
 };
 
 #endif // Graph_H
